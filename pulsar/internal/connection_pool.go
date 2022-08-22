06cb97711ca3aa6f1f7eab2c4bb644de8eae7d5e
billowqiu <billowqiu@163.com>
2022-08-22 10:04:30 +0800

    add cnx ID log

--- pulsar/internal/connection_pool.go
+++ pulsar/internal/connection_pool.go
@@ -80,8 +80,8 @@ func (p *connectionPool) GetConnection(logicalAddr *url.URL, physicalAddr *url.U
 
 		// remove stale/failed connection
 		if conn.closed() {
-			p.log.Infof("Removed connection from pool key=%s logical_addr=%+v physical_addr=%+v",
-				key, conn.logicalAddr, conn.physicalAddr)
+			p.log.Infof("Connection state closed %s, Removed connection from pool key=%s logical_addr=%+v physical_addr=%+v",
+				conn.ID(), key, conn.logicalAddr, conn.physicalAddr)
 			delete(p.connections, key)
 			conn.Close()
 			conn = nil // set to nil so we create a new one
