74e45cd94d2ddf97114ffbba9b52ca198fcdf2fd
billowqiu <billowqiu@163.com>
2022-12-12 18:00:41 +0800

    revert Send context timeout in gw

--- pulsar/producer_partition.go
+++ pulsar/producer_partition.go
@@ -786,13 +786,8 @@ func (p *partitionProducer) Send(ctx context.Context, msg *ProducerMessage) (Mes
 	}, true)
 
 	// wait for send request to finish
-	select {
-	case <-ctx.Done():
-		isDone.Store(true)
-		return msgID, ctx.Err()
-	case <-doneCh:
-		return msgID, err
-	}
+	<-doneCh
+	return msgID, err
 }
 
 func (p *partitionProducer) SendAsync(ctx context.Context, msg *ProducerMessage,
